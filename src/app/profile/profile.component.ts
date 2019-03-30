24224782b64ff6ad75c9035dd9a4c183961f33ab
spgdaman <gatheru.simon432@gmail.com>
2019-03-30 12:49:03 +0300

    add interface, use promise to make requests to the github api

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  } from '../services/profile';
 
 @Component({
   selector: 'app-profile',
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Users } from '../users';
 import { Repositories } from '../repositories';
@@ -13,8 +13,8 @@ export class ProfileService {
   username: Users;
   repository: Repositories;
 
-  constructor(private http: Http) {
-    console.log("Service is now ready");
+  constructor(private http: HttpClient) {
+    console.log('Service is now ready');
     this.username = 'spgdaman';
   }
 
@@ -25,7 +25,16 @@ export class ProfileService {
       login: string;
       name: string;
     }
-    return this.http.get("https://api.github.com/users/" + this.username)
-    .map(res => res.json());
+
+    let promise = new Promise((resolve, reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl + this.username).toPromise().then(response=>{
+        resolve()
+      },
+      error=>{
+
+      })
+    })
+
+    return promise;
   }
 }

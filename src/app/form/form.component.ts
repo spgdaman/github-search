c0ae5a06bd28b36f268a683b3aedecefa484c311
spgdaman <gatheru.simon432@gmail.com>
2019-03-31 22:08:23 +0300

    commit before push

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Users } from './users';
+import { Users } from '../users';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
+
 export class FormComponent implements OnInit {
 
   newUsername = new Users('');

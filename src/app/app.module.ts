07141091f8d2038b7f4909e5992c8ec70d8373f1
spgdaman <gatheru.simon432@gmail.com>
2019-03-31 06:48:23 +0300

    generate form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FormComponent } from './form/form.component';
 // import { ProfileService } from './services/profile.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    FormComponent,
     // HttpClientModule
   ],
   imports: [
